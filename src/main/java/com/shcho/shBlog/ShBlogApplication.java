b3fe3791d1a5cd0ff3cc73446520f68cbdaebac6
sunghyun <csh980116@naver.com>
2025-07-17 18:12:17 +0900

    feat : Project init

---
+++ src/main/java/com/shcho/shBlog/ShBlogApplication.java
@@ -0,0 +1,13 @@
+package com.shcho.shBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shcho/shBlog/ShBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shcho.shBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

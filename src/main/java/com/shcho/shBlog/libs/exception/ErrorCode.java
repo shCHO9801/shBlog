82ec96585cf94ee9944e80f5a158fc9f2718e8bc
sunghyun <csh980116@naver.com>
2025-07-29 17:44:43 +0900

    feat : User SignIn 서비스 로직에 해당하는 ErrorCode 추가

--- src/main/java/com/shcho/shBlog/libs/exception/ErrorCode.java
+++ src/main/java/com/shcho/shBlog/libs/exception/ErrorCode.java
@@ -11,14 +11,17 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "USER_001", "유저를 찾을 수 없습니다."),
     ALREADY_DELETED_USER(404, "USER_002", "이미 삭제된 유저 입니다."),
 
-    /* 409 Conflict*/
-    DUPLICATED_USERNAME(409, "USER_003","이미 사용 중인 아이디 입니다."),
+    /* 409 CONFLICT */
+    DUPLICATED_USERNAME(409, "USER_003", "이미 사용 중인 아이디 입니다."),
     DUPLICATED_EMAIL(409, "USER_004", "이미 사용 중인 이메일입니다."),
     DUPLICATED_NICKNAME(409, "USER_005", "이미 사용 중인 닉네임입니다."),
 
-    /* 500 INTERNAL_SERVER_ERROR */
-    INTERNAL_SERVER_ERROR(500, "COMMON_500", "서버 오류가 발생했습니다.");
+    /* 401 UNAUTHORIZED */
+    INVALID_USERNAME_OR_PASSWORD(401, "AUTH_001", "아이디 또는 비밀번호가 올바르지 않습니다."),
 
+    /* 500 INTERNAL_SERVER_ERROR */
+    INTERNAL_SERVER_ERROR(500, "COMMON_500", "서버 오류가 발생했습니다."),
+    JWT_KEY_ERROR(500, "AUTH_500", "JWT 키가 유효하지 않습니다.");
 
     private final Integer httpStatus;
     private final String code;

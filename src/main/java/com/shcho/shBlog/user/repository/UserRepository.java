c0da99bd7dda69d6cd0068f83c65d4324299ff5b
sunghyun <csh980116@naver.com>
2025-07-29 17:59:02 +0900

    feat : CustomUserDetails 와 CustomUserDetailsService 구현

--- src/main/java/com/shcho/shBlog/user/repository/UserRepository.java
+++ src/main/java/com/shcho/shBlog/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.shcho.shBlog.user.repository;
 import com.shcho.shBlog.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickname(String nickname);
 
     User getReferenceByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }

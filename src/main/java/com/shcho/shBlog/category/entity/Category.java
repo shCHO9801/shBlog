83dbbb8a25e4ef0d2e595b9ffdfee2d78ce39946
sunghyun <csh980116@naver.com>
2025-09-24 19:02:03 +0900

    feat : Category 엔티티 설계

---
+++ src/main/java/com/shcho/shBlog/category/entity/Category.java
@@ -0,0 +1,25 @@
+package com.shcho.shBlog.category.entity;
+
+import com.shcho.shBlog.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    private String description;
+}
